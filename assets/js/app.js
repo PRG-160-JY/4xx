22aea5b6d3166ecb3b3dab8327adae6e9c8ffad3
JoseYepez <yepez.jose719@gmail.com>
2018-04-20 22:06:24 -0400

    We added the functionality for the application interface. Additionally we added styles for the interface as well. We also set all resource links to a passive protocol.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,8 @@
-// JavaScript Document
 'use strict';
 
 var appData = {
 	title: 'inside out - js',
-	tagLine: 'continuously looking forward into the light...'
-
-
-
+	tagLine: 'continuously falling forward into the light...'
 };
 
 window.addEventListener('load', initializeApplication);
@@ -20,30 +16,27 @@ function initializeApplication() {
 	document.body.style.fontSize = '3em';
 
 	var elWrapper = document.createElement('div');
-	elWrapper.id = 'appliactionWrapper';
+	elWrapper.id = 'applicationWrapper';
 	document.body.appendChild(elWrapper);
-
 
 	var elHeader = document.createElement('header');
 	elWrapper.appendChild(elHeader);
 
 	var elHeaderTitle = document.createElement('h1');
 	elHeaderTitle.innerHTML = appData.title;
-	elHeaderTitle.className = 'animated bounceInDown';
 	elHeader.appendChild(elHeaderTitle);
+	elHeaderTitle.className = 'animated bounceInDown';
 
-	var elHeaderTagline = document.createElement('h2');
-	elHeaderTagline.textContent = appData.tagLine;
-	elHeaderTagline.style.fontSize = '.4em';
-	elHeader.appendChild(elHeaderTagline);
-	elHeaderTagline.className = 'animated bounceInLeft';
-
+	var elHeaderTagLine = document.createElement('h2');
+	elHeaderTagLine.textContent = appData.tagLine;
+	elHeaderTagLine.style.fontSize = '.4em';
+	elHeader.appendChild(elHeaderTagLine);
+	elHeaderTagLine.className = 'animated bounceInLeft';
 
 	var elMain = document.createElement('main');
 	elMain.innerHTML = '<p>Hello World!</p>';
 	elWrapper.appendChild(elMain);
 	elMain.className = 'animated zoomIn';
-
 
 	var elFooter = document.createElement('footer');
 	elFooter.innerHTML = '<h4>get excited about learning JavaScript...</h4>';
@@ -53,9 +46,7 @@ function initializeApplication() {
 	elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px; margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
 
 	displayPB();
-
 }
-
 
 var timerCount = 0;
 
@@ -70,17 +61,13 @@ function displayPB() {
 	} else {
 		timerCount = 0;
 
-		// start step 402-0
+		//start step-402
 		document.body.innerHTML = formLogin();
-		// end step 402-0
-
-
+		//end step-402
 
 		return false;
 	}
 }
-
-// start step 402-0
 
 function formLogin() {
 	return '<form id="loginFrm" name="loginFrm" action="#" class="animated zoomIn"><div class="form-group"><label for="email">Email address:</label><input type="email" class="form-control" id="email" value="me@there.com"></div><div class="form-group"><label for="pwd">Password:</label><input type="password" class="form-control" id="pwd" value="dfdf"></div><div class="form-check"><label class="form-check-label"><input class="form-check-input" type="checkbox">Remember me </label></div><button id="loginBtn" type="submit" class="btn btn-primary" onclick="validateLogin()">Submit</button></form>';
@@ -90,11 +77,37 @@ function validateLogin() {
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("pwd").value;
 	if (email !== '' && password !== '') {
-		document.body.innerHTML = '<main class="animated zoomIn"><h1 id="inNow">We are in now</h1></main>';
+		//document.body.innerHTML = '<main class="animated zoomIn"><h1 id="inNow">We are in now</h1></main>';
+		applicationUserInterface();
 	} else {
 		alert('bad');
 	}
 	return false;
 }
 
-// end step 402-0
+function applicationUserInterface() {
+	let header = '<div class="container-fluid"><div class="row"><div class="col-12"><nav id="header" class="navbar navbar-dark fixed-top bg-dark"><a class="navbar-brand" data-dest="logoLink"  href="#">inside out</a></nav></div></div>';
+	let main = '<div class="row contStage"><div class="col-2">' + buildMenu() + '</div><div class="col-10">' + buildMain() + '</div></div>';
+	let footer = '<div class="row"><div class="col-12"><nav id="footer" class="navbar fixed-bottom navbar-dark bg-dark"></nav></div></div></div>';
+	document.body.style.backgroundColor = '#FFF';
+	document.body.innerHTML = header + main + footer;
+
+	var links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) links[i].onclick = function () {
+		linkClicked(this);
+	}
+}
+
+function buildMenu() {
+	return '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column"> <li class="nav-item"> <a class="nav-link active" data-dest="link01" href="#">Active</a> </li><li class="nav-item"> <a class="nav-link" data-dest="link02"  href="#">Link</a> </li><li class="nav-item"> <a class="nav-link" data-dest="link03" href="#">Link</a> </li><li class="nav-item"> <a class="nav-link disabled" data-dest="link04" href="#">Disabled</a> </li></ul></nav>';
+}
+
+function buildMain() {
+	return '<main id="main" role="main"><h1 class="animated zoomIn">Main Content</h1></main>';
+}
+
+function linkClicked(obj) {
+	console.log(obj.dataset.dest);
+	var str = '<h1 class="animated zoomIn">' + obj.dataset.dest + ' was clicked!</h1>';
+	document.getElementById("main").innerHTML = str.repeat(20);
+}
